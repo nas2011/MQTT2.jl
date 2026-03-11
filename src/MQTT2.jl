963405fa9811a52d40daefc3989d93d6e70e4cee
nas2011 <101505861+nas2011@users.noreply.github.com>
2026-03-10 21:59:56 -0400

    Create MQTT2.jl
    initial

---
+++ src/MQTT2.jl
@@ -0,0 +1,18 @@
+module MQTT2
+
+using Sockets
+
+include("utils.jl")
+include("structs.jl")
+include("protocol.jl")
+include("client.jl")
+
+export QoS, qos0, qos1, qos2, 
+       Properties, ConnectPacket, ConnackPacket, PublishPacket, PubackPacket, 
+       PubrecPacket, PubrelPacket, PubcompPacket, SubscribePacket, SubackPacket, 
+       UnsubscribePacket, UnsubackPacket, PingreqPacket, PingrespPacket, 
+       DisconnectPacket, AuthPacket, Subscription
+export encodePacket, decodePacket
+export MqttClient,ClientConfig, connect!, publish!, subscribe!, disconnect!
+
+end
